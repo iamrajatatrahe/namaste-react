9acbacc29d66fd7d95a1b3d01c1cc6893aec147b
iamrajatatrahe <iamrajatatrahe@gmail.com>
2025-10-07 18:17:18 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const AppLayout = () => {
   useEffect(() => {
     // Make an API call and send username and password
     const data = {
-      name: "Rajat Atrahe",
+      name: "Akshay Saini",
     };
     setUserName(data.name);
   }, []);
